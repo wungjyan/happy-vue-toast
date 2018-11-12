3afcde24839cb686b1e4e80addca610dde7bb12a
wungjyan <1170017412@qq.com>
2018-11-12 10:03:10 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+module.exports = {
+    entry: './lib/index.js',
+    mode:'production',
+    output:{
+        path: path.resolve(__dirname,'./dist'),
+        filename: 'happy-vue-toast.js',
+        library:'happy-vue-toast',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                loader:'babel-loader',
+                exclude:/node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'vue-style-loader',
+                  'css-loader'
+                ]
+              }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugin()
+    ]
+}
